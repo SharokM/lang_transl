68d89d77660201594f8ab4af6557c133fdb57995
sharok_m <sharok.d.mcdonnaugh@gmail.com>
2025-12-04 15:26:27 +0000

    loop added

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 translations = {
   "hello":"hola",
   "thank you":"gracias",
-  "sorry":"lo siento"
+  "sorry":"lo siento",
+  "connect":"instalar",
+  "save":"guardar",
+  "scroll":"desplazar",
+  "paste":"lo pegar",
+  "close":"cerrar",
+  "open":"abrir"
 }
 
 done = False
 
-print('Type "done" at any time to exit')
+print('Type "exit" at any time to exit')
+
+while not done:
+    word = input("Enter a word to translate (hello, thank you, sorry): ").strip().lower()
+    if word == "exit":
+        done = True
+    elif word in translations:
+        print(f'The translation of "{word}" is "{translations[word]}"')
+    else:
+        print(f'"{word}" is not currently in our dictionary, thank you for your patience while we update our vocabulary database.')
+
+
