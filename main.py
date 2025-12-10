b905fe90024ee15d0e800728e7912ab2c156d0a5
sharok_m <sharok.d.mcdonnaugh@gmail.com>
2025-12-10 16:51:21 +0000

    added csv trans file

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
+import csv
+
 translations = {
-  "hello":"hola",
-  "thank you":"gracias",
-  "sorry":"lo siento",
-  "connect":"instalar",
-  "save":"guardar",
-  "scroll":"desplazar",
-  "paste":"lo pegar",
-  "close":"cerrar",
-  "open":"abrir"
+
 }
+
+with open("translations.csv", "r") as words:
+    reader = csv.DictReader(words, delimiter=",")
+    for line in reader:
+        english = line["English"].lower()
+        spanish = line["Spanish"].lower()
+        french = line["French"].lower()
+        translations[english] = [spanish, french]
+        print(line["English"], line["French"], line["Spanish"])
+
 
 done = False
 
